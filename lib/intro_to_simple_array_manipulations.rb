c0656af6a2ad3f9c77300e6a6afaab43cc734350
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-06-25 06:18:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(updated_array, new_neighborhood)
+  updated_array.unshift(new_neighborhood)
+end
+
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift(im_so_over_this_city)
+  im_so_over_this_city.shift
+end
+
+def shift_with_args(brands_removed)
+  brands_removed.shift(2)
+end
+
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(new_array, list_of_esoteric_programming_languages)
+  new_array.insert(4, list_of_esoteric_programming_languages)
+end
+
+def using_uniq(new_array)
+   new_array.uniq
+ end
+
+def using_flatten(flat_array)
+  flat_array.flatten
+end
+
+def using_delete(instructions, no_offense_steven)
+  instructions.delete(no_offense_steven)
+end
+
+def using_delete_at(famous_robots, int)
+  famous_robots.delete_at(int)
+end
